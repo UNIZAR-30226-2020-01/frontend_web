a707bea9a9dd32853199d50952941ac1bd3ad66e
chus_ejea <jesusvillacampasagaste@gmail.com>
2020-04-04 13:06:22 +0200

    empezamos vue

---
+++ register.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+    el: '#form',
+    data:{
+        vusername: '',
+        vemail: '',
+        vpassword: '',
+        vconfpasswd: ''
+
+    },
+    methods:{
+        agregarusuario: function() {
+            console.log(this.vusername);
+            console.log(this.vemail);
+            console.log(this.vpassword);
+            console.log(this.vconfpasswd);
+            if(this.vpassword === this.vconfpasswd){
+                //meter a la base de datos
+            }else{
+                console.log('error');
+            }
+            
+        }
+    }
+
+});
