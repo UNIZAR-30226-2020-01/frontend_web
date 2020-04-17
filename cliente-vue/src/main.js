2ff82edf372364e3ef50b16356b835fca0d5dc3e
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-04-17 12:03:17 +0200

    Obligacion al usuario de que este bloqueado antes de acceder a cualquier página de la web

--- cliente-vue/src/main.js
+++ cliente-vue/src/main.js
@@ -18,6 +18,25 @@ const router = new VueRouter({
   mode: 'hash'
 });
 
+router.beforeEach(
+  function(to, from, next) {
+    // console.log(to)
+    if(to.path !== "/login" && to.path !== "/register"){
+      // Antes de cambiar a cualquier localizacion comprobamos si el usuario esta logueado
+      if(localStorage.getItem('token') != "null"){
+        console.log("Hay token");
+        next();
+      }else{
+        console.log("No hay token");
+        next({path: '/login', replace: false});
+      }
+    }else{
+      // Permitimos el acceso sin token al registro o a iniciar sesion
+      next();
+    }
+  }
+);
+
 Vue.config.productionTip = false
 
 new Vue({
