3853afbdaf4749565f0b1cd3fb2d84c11e7eb889
chus_ejea <jesusvillacampasagaste@gmail.com>
2020-04-27 19:14:40 +0200

    Arreglada la pantalla de playlist

--- cliente-vue/src/components/albums.vue
+++ cliente-vue/src/components/albums.vue
@@ -1,40 +1,4 @@
 <template>
-<!--
-  <div id="content">
-    <div class="container" id="content-container">
-      <div class="artists">
-        <ul class="list-inline-item text-center">
-          <li class="d-table-row album-item" v-for="album in albums" :key="album.title">
-            <router-link v-bind:to="'/albums/' + album.id ">
-            <div class="album-div">
-              <img class="album-pic" alt="album_pic" v-bind:src="album.icon">
-                <div id="x">
-                  <h6 class="album-name" style="color: white">{{ album.title }}</h6>
-                  <h6 class="album-artist">{{ album.artist.name }}</h6>
-                </div>
-            </div>
-          </router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
--->
-
-<!-- <li class="list-inline-item artist-item" v-for="artist in artists" :key="artist.name">
-  <router-link v-bind:to="'/artists/' + artist.id ">
-  <div class="card text-center p-2 artist-card" >
-    <header class="card-header">
-      <h3 class="card-title artist-name" style="font-size: 16px">{{artist.name}}</h3>
-      <h6 class="card-subtitle">Artist</h6>
-    </header>
-    <div class="card-body"><img id="artist-pic" class="m-auto mx-auto d-block" v-bind:src="artist.image">
-      <p id="artist-songs" class="card-text artist-songs d-inline">{{artist.number_songs}} songs</p>
-      <p class="card-text artist-albums d-inline">&nbsp;{{artist.number_albums}} albums</p>
-    </div>
-  </div>
-</router-link>
-</li> -->
 
   <div class="col">
     <div class="container-fluid">
@@ -55,7 +19,6 @@
         </ul>
     </div>
 </div>
-
 
 </template>
 
--- cliente-vue/src/components/playlists.vue
+++ cliente-vue/src/components/playlists.vue
@@ -1,36 +1,34 @@
 <template>
 
-  <!-- <div id="content">
-    <div class="container" id="content-container">
-      <div class="artists">
-        <ul class="list-unstyled">
-          <li class="artist-item">
-            <div class="artist-div" v-for="artist in artists" :key="artist.name">
-              <router-link v-bind:to="'/artists/' + artist.id ">
-                <img class="artist-pic" alt="artist_pic" v-bind:src="artist.image" />
-                <div id="x">
-                  <h2 class="artist-name">{{artist.name}}</h2>
-                  <h6>Artist</h6>
-                  <p>{{artist.number_albums}} albums</p>
-                  <p>{{artist.number_songs}} songs</p>
-                </div>
-              </router-link>
+  <!-- <div class="col">
+    <div class="container-fluid">
+      <ul class="list-inline text-center">
+        <li class="list-inline-item artist-item" v-for="artist in artists" :key="artist.name">
+          <router-link v-bind:to="'/artists/' + artist.id ">
+          <div class="card text-center p-2 artist-card" >
+            <header class="card-header">
+              <h3 class="card-title artist-name" style="font-size: 16px">{{artist.name}}</h3>
+              <h6 class="card-subtitle">Artist</h6>
+            </header>
+            <div class="card-body"><img id="artist-pic" class="m-auto mx-auto d-block" v-bind:src="artist.image">
+              <p id="artist-songs" class="card-text artist-songs d-inline">{{artist.number_songs}} songs</p>
+              <p class="card-text artist-albums d-inline">&nbsp;{{artist.number_albums}} albums</p>
             </div>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </router-link>
+        </li>
+      </ul>
     </div>
-  </div> -->
-<div>
+</div> -->
+
   <div class="col">
     <div class="container-fluid">
-      <div class="genre">
         <ul class="list-inline text-center">
           <li class="list-inline-item artist-item" v-for="playlist in playlists" :key="playlist.name">
             <router-link v-bind:to="'/playlists/' + playlist.id ">
             <div class="card text-center p-2 artist-card" >
               <header class="card-header">
-                <h3 class="card-title artist-name">{{ playlist.title }}</h3>
+                <h3 class="card-title artist-name" style="font-size: 90%" >{{ playlist.title }}</h3>
                 <h6 class="card-subtitle">Artist</h6>
               </header>
               <div class="card-body"><img id="artist-pic" class="m-auto mx-auto d-block" v-bind:src="playlist.icon">
@@ -42,10 +40,8 @@
           </li>
         </ul>
       </div>
-    </div>
-  </div>
-  <!-- <player></player> -->
 </div>
+
 </template>
 
 <script>
