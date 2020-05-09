8be8fde622409b054c49e34ecc8fd7c0a513f676
Haz99 <jsalamerosanz@gmail.com>
2020-05-09 19:40:51 +0200

    Actualizar barraLateral.vue

--- cliente-vue/src/components/barraLateral.vue
+++ cliente-vue/src/components/barraLateral.vue
@@ -4,23 +4,17 @@
         <ul class="list-unstyled">
           <li><router-link to="/inicio">Home</router-link></li>
           <li>Search</li>
-          <li><router-link to="/playlists">Your library</router-link></li>
+          <li v-if="!mostrarMenuPodcasts"><router-link to="/playlists">Your library</router-link></li>
+          <li @click=changeMenu()>
+            <div v-if="mostrarMenuPodcasts">
+              <router-link to="/newPodcasts">Podcasts</router-link>
+            </div>
+            <div v-else>
+              <router-link to="/playlists">Songs</router-link>
+            </div>
+          </li>
         </ul>
         <div>
-          <div @click=changeMenu()  v-if="mostrarMenuPodcasts" class="dropdown"><button class="btn dropdown-toggle" data-toggle="dropdown" aria-expanded="false" id="podcasts-dd" type="button"><router-link to="/playlists" id="podcasts-dd">Songs</router-link></button>
-            <div class="dropdown-menu" role="menu">
-              <a class="dropdown-item" role="presentation" href="#">New Podcasts</a>
-              <a class="dropdown-item" role="presentation" href="#">Subscriptions</a>
-              <a class="dropdown-item" role="presentation" href="#">Discover</a>
-            </div>
-          </div>
-          <div @click=changeMenu() v-else class="dropdown"><button class="btn dropdown-toggle" data-toggle="dropdown" aria-expanded="false" id="podcasts-dd" type="button"><router-link to="/newPodcasts" id="podcasts-dd">Podcasts</router-link></button>
-              <div class="dropdown-menu" role="menu">
-                <a class="dropdown-item" role="presentation" href="#">New Podcasts</a>
-                <a class="dropdown-item" role="presentation" href="#">Subscriptions</a>
-                <a class="dropdown-item" role="presentation" href="#">Discover</a>
-              </div>
-          </div>
         </div>
         <button class="btn btn-primary fixed-bottom" type="button" v-on:click="open_player_btn"><i class="fas fa-music" style="color: rgb(0,0,0);"></i></button>
       </div>
