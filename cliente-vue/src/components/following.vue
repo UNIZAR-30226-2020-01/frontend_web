e0852dff8d791cb5104c5792a8867a7295b55d65
chus_ejea <jesusvillacampasagaste@gmail.com>
2020-05-21 01:24:41 +0200

    Primera versión pantalla Following

--- cliente-vue/src/components/following.vue
+++ cliente-vue/src/components/following.vue
@@ -4,15 +4,15 @@
     <div class="container-fluid">
       <ul class="list-inline text-center">
         <li class="list-inline-item artist-item" v-for="playlist in playlistFollowed" :key="playlist.name">
-          <router-link v-bind:to="'/playlist/' + playlist.id ">
+          <router-link v-bind:to="'/playlists/' + playlist.id ">
           <div class="card text-center p-2 artist-card" >
             <header class="card-header">
-              <h3 class="card-title artist-name" style="font-size: 16px">{{artist.name}}</h3>
-              <h6 class="card-subtitle">Artist</h6>
+              <h3 class="card-title artist-name" style="font-size: 100%">{{playlist.title}}</h3>
+              <h6 class="card-subtitle">Playlist</h6>
             </header>
-            <div class="card-body"><img id="artist-pic" class="m-auto mx-auto d-block" v-bind:src="artist.image">
-              <p id="artist-songs" class="card-text artist-songs d-inline">{{artist.number_songs}} songs</p>
-              <p class="card-text artist-albums d-inline">&nbsp;{{artist.number_albums}} albums</p>
+            <div class="card-body"><img id="artist-pic" class="m-auto mx-auto d-block" v-bind:src="playlist.icon">
+              <p id="artist-songs" class="card-text artist-songs d-inline">{{playlist.number_songs}} songs</p><br>
+              <p class="card-text artist-albums d-inline"> User: {{playlist.user.username}}</p>
             </div>
           </div>
         </router-link>
@@ -26,31 +26,22 @@
 
 <script>
   // import Player from './player.vue'
-  import followingMixin from '../mixins/followingActions'
+  import followingMixin from '../mixins/followingActions';
   export default {
     data() {
       return {
         playlistFollowed: []
-      }
+      };
     },
     // components:{
     //   'player': Player,
     // },
     mixins: [followingMixin],
     created() {
-      this.playlistFollowed = this.getFollowingPlaylists
-      console.log(this.getFollowingPlaylists)
+      this.getFollowingPlaylists
       console.log("yaaaaaaaaaaaaaaaa")
       console.log(this.playlistFollowed);
     },
-    methods: {
-      getId: function(artist) {
-        let list = artist.url.split('/')
-        console.log(list)
-        let id = list[list.length - 1]
-        return id
-      }
-    }
   }
 
 </script>
--- cliente-vue/src/mixins/followingActions.js
+++ cliente-vue/src/mixins/followingActions.js
@@ -34,56 +34,64 @@ export default {
                 }
             );
         },
-        getFollowingPlaylists: function () {
-            this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/user/followed/playlists/', {
-                headers: {
-                    Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
-                }
-            }).then(
-                function (response) {
-                    if (response.status == 200) {
-                        // Ha ido bien. Devolvemos la lista de playlist de los usuarios a los que seguimos
-                        return response.body;
-                    } else {
-                        // Ha habido un error
-                        console.log("Ha habido un error al recuperar las playlist de los seguidos. Codigo de error: " + response.status);
-                    }
-                }
-            );
-        },
-        getFollowingUsers: function () {
-            this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/current-user/', {
-                headers: {
-                    Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
-                }
-            }).then(
-                function (response) {
-                    if (response.status == 200) {
-                        // Ha ido bien, devolvemos la lista de usuarios a los que seguimos
-                        return response.body[0].following;
-                    } else {
-                        // Ha habido un error
-                        console.log("Ha habido un error al recuperar los usuarios seguidos. Codigo de error: " + response.status);
-                    }
-                }
-            );
-        },
-        getFollowerUsers: function () {
-            this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/current-user/', {
-                headers: {
-                    Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
-                }
-            }).then(
-                function (response) {
-                    if (response.status == 200) {
-                        // Ha ido bien, devolvemos la lista de usuarios a los que seguimos
-                        return response.body[0].followers;
-                    } else {
-                        // Ha habido un error
-                        console.log("Ha habido un error al recuperar los usuarios seguidores. Codigo de error: " + response.status);
-                    }
-                }
-            );
-        }
+    },
+    computed:{
+      getFollowingPlaylists: function () {
+          this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/user/followed/playlists/', {
+              headers: {
+                  Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
+              }
+          }).then(
+              function (response) {
+                  if (response.status == 200) {
+                      // Ha ido bien. Devolvemos la lista de playlist de los usuarios a los que seguimos
+                      this.playlistFollowed = response.body;
+                      this.playlistFollowed.forEach((podcast) => {
+                        var list = podcast.url.split('/');
+                        console.log(list);
+                        podcast.id = list[list.length - 2];
+                        podcast.url.replace('http://', 'https://');
+                      });
+                  } else {
+                      // Ha habido un error
+                      console.log("Ha habido un error al recuperar las playlist de los seguidos. Codigo de error: " + response.status);
+                  }
+              }
+          );
+      },
+      getFollowingUsers: function () {
+          this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/current-user/', {
+              headers: {
+                  Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
+              }
+          }).then(
+              function (response) {
+                  if (response.status == 200) {
+                      // Ha ido bien, devolvemos la lista de usuarios a los que seguimos
+                      return response.body[0].following;
+                  } else {
+                      // Ha habido un error
+                      console.log("Ha habido un error al recuperar los usuarios seguidos. Codigo de error: " + response.status);
+                  }
+              }
+          );
+      },
+      getFollowerUsers: function () {
+          this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/current-user/', {
+              headers: {
+                  Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
+              }
+          }).then(
+              function (response) {
+                  if (response.status == 200) {
+                      // Ha ido bien, devolvemos la lista de usuarios a los que seguimos
+                      return response.body[0].followers;
+                  } else {
+                      // Ha habido un error
+                      console.log("Ha habido un error al recuperar los usuarios seguidores. Codigo de error: " + response.status);
+                  }
+              }
+          );
+      }
     }
 }
