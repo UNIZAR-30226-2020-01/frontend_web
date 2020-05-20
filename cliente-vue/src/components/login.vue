6312e0a337c5f1b16a0b93daa93491a24271d18d
nestor <nestormg98@gmail.com>
2020-05-20 16:40:56 +0200

    Doble de anchura a lo de Google

--- cliente-vue/src/components/login.vue
+++ cliente-vue/src/components/login.vue
@@ -49,7 +49,7 @@
         },
         // only needed if you want to render the button with the google ui
         renderParams: {
-            width: 250,
+            width: 500,
             height: 50,
             longtitle: true
         },
