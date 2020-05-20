a9a29223c8072e8df277dcd847c94f4df6334ce3
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-05-20 13:16:58 +0200

    [skip ci] Actualización de funciones y añadido la de obtener usuarios seguidos.

--- cliente-vue/src/mixins/followingActions.js
+++ cliente-vue/src/mixins/followingActions.js
@@ -1,52 +1,69 @@
 export default {
     methods: {
-        followUser: function(user){
+        followUser: function (user) {
             this.$http.get(user.url + 'follow/', {
                 headers: {
                     Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
                 }
             }).then(
-                function(response) {
-                    if(response.status == 200){
+                function (response) {
+                    if (response.status == 200) {
                         // Ha ido bien. Se está siguiendo al usuario <user>
                         console.log("Se ha empezado a seguir al usuario " + user.username);
-                    }else{
+                    } else {
                         // Error
                         console.log("Se ha producido un error al seguir al usuario " + user.username + ". Codigo de error: " + response.status);
                     }
                 }
             );
         },
-        unfollowUser: function(user) {
+        unfollowUser: function (user) {
             this.$http.get(user.url + 'unfollow/', {
                 headers: {
                     Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
                 }
             }).then(
-                function(response) {
-                    if(response.status == 200){
+                function (response) {
+                    if (response.status == 200) {
                         // Ha ido bien. Se está siguiendo al usuario <user>
                         console.log("Se ha dejado de seguir al usuario " + user.username);
-                    }else{
+                    } else {
                         // Error
                         console.log("Se ha producido un error al dejar de seguir al usuario " + user.username + ". Codigo de error: " + response.status);
                     }
                 }
             );
         },
-        getFollowingPlaylists: function(){
-            this.$http.get('http://s7-rest.francecentral.cloudapp.azure.com/user/followed/playlists/', {
+        getFollowingPlaylists: function () {
+            this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/user/followed/playlists/', {
                 headers: {
                     Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
                 }
             }).then(
-                function(response) {
-                    if(response.status == 200){
+                function (response) {
+                    if (response.status == 200) {
                         // Ha ido bien. Devolvemos la lista de playlist de los usuarios a los que seguimos
                         return response.body;
-                    }else{
+                    } else {
                         // Ha habido un error
                         console.log("Ha habido un error al recuperar las playlist de los seguidos. Codigo de error: " + response.status);
+                    }
+                }
+            );
+        },
+        getFollowingUsers: function () {
+            this.$http.get('https://s7-rest.francecentral.cloudapp.azure.com/current-user/', {
+                headers: {
+                    Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
+                }
+            }).then(
+                function (response) {
+                    if (response.status == 200) {
+                        // Ha ido bien, devolvemos la lista de usuarios a los que seguimos
+                        return response.body.following;
+                    } else {
+                        // Ha habido un error
+                        console.log("Ha habido un error al recuperar los usuarios seguidos. Codigo de error: " + response.status);
                     }
                 }
             );
