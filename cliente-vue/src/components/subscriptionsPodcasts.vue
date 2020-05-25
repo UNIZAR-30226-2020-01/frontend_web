f7aed94e7bbd0387fdb4961f4be67b84e869e1d4
chus_ejea <jesusvillacampasagaste@gmail.com>
2020-05-25 22:57:35 +0200

    Cambiado el router link de los podcast

--- cliente-vue/src/components/subscriptionsPodcasts.vue
+++ cliente-vue/src/components/subscriptionsPodcasts.vue
@@ -8,17 +8,17 @@
           </div>
           <ul class="list-inline">
               <li class="list-inline-item m-1" v-for="podcast in podcasts" :key="podcast.title" >
-                <router-link v-bind:to="'/subscriptionsPodcasts/' + podcast.id ">
                   <div class="card m-2" id="subscribed-podcast-card">
                       <div class="card-body" id="subscribed_podc"><img id="subscribed_podc_cover" class="m-2 rounded" v-bind:src="podcast.image">
                           <div id="div_subs_text">
+                            <router-link v-bind:to="'/subscriptionsPodcasts/' + podcast.id ">
                               <h4 id="links_PodcastsSeries">{{podcast.title}}</h4>
                               <h6 id="links_PodcastsSeries">{{podcast.channel.name}}</h6>
                               <h6 id="links_PodcastsSeries">{{podcast.number_episodes}} episodes</h6>
+                            </router-link>
                               <button class="btn btn-primary black subsPodcastSubBtn" id="unsub_btn" type="button">Unsubscribe&nbsp;<i class="fa fa-minus" style="in"></i></button>
                           </div>
                       </div></div>
-                </router-link>
               </li>
           </ul>
         </div>
