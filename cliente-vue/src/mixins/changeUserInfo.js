4f4566e645ff6753b8ad7725a39e5d0210638b05
Pedro Tamargo <pedro.tamargo.allue@gmail.com>
2020-05-27 15:14:16 +0200

    Cambiar imagen listo [skip ci]

---
+++ cliente-vue/src/mixins/changeUserInfo.js
@@ -0,0 +1,23 @@
+export default {
+    methods: {
+        changeUserImage: function(image) {
+            let fd = new FormData();
+            fd.append('icon', image);
+
+            this.$http.put('https://s7-rest.francecentral.cloudapp.azure.com/update-user/', fd, {
+                headers: {
+                    Authorization: localStorage.getItem('type') + ' ' + localStorage.getItem('token'),
+                }
+            }).then(
+                function(response) {
+                    if(response.status == 200){
+                        // Ha ido bien
+                        console.log("Se ha modificado la imagen del usuario");
+                        // Esto nos devuelve el usuario
+                        console.log(response.status);
+                    }
+                }
+            );
+        }
+    }
+}
